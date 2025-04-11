5c52ef341c12fb5917eb282db5d35acd40da14f6
youngrongoh <youngrong.oh@gmail.com>
2025-04-11 23:13:29 +0900

    feat. react 실행 확인

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div>React App</div>
+  </StrictMode>,
+)
