b9416a63a75ea74ed0f88d3db53b2655413cb33b
youngrongoh <youngrong.oh@gmail.com>
2025-04-14 21:52:41 +0900

    feat. react-router 셋업

---
+++ src/router/routes.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div className="text-xl bg-blue-300">React App</div>,
+  },
+]);
+
+export default router;
