947c361d2c95bea8658324c27f55708a1cd4e7f6
youngrongoh <youngrong.oh@gmail.com>
2025-04-14 17:55:49 +0900

    chore. shadcn설치 전 셋업

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   root: 'src',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
