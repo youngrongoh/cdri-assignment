d39e52fa7c3237b0ce179d5b233b88ac1bb03f39
youngrongoh <youngrong.oh@gmail.com>
2025-04-15 19:26:41 +0900

    chore. svgr 셋업

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
+declare module '*.svg' {
+  const content: React.FC<React.SVGProps<SVGElement>>;
+  export default content;
+}
+
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      include: '**/*.svg',
+    }),
+  ],
   root: 'src',
   resolve: {
     alias: {
