dd673238fb7eb3b41101d48810efbb4f1b057423
youngrongoh <youngrong.oh@gmail.com>
2025-04-16 17:14:42 +0900

    feat. query client 셋업

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,15 +1,19 @@
 import { Outlet } from 'react-router';
 import GNB from '@/components/GNB';
+import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from '@/lib/query-client';
 
 export default function Layout() {
   return (
-    <div className="min-h-screen">
-      <header>
-        <GNB />
-      </header>
-      <main className="mx-auto py-8">
-        <Outlet />
-      </main>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="min-h-screen">
+        <header>
+          <GNB />
+        </header>
+        <main className="mx-auto py-8">
+          <Outlet />
+        </main>
+      </div>
+    </QueryClientProvider>
   );
 }
